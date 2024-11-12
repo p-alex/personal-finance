7632c00acd469a17103fa9e76b0c688a66396d11
p-alex <alex.daniel9021@gmail.com>
2024-11-12 15:22:43 +0200

    Added one more test to ParamExtractor

--- packages/server/src/utils/ParamExtractor.ts
+++ packages/server/src/utils/ParamExtractor.ts
@@ -1,8 +1,11 @@
 class ParamExtractor {
-  execute(pattern: string, url: string) {
-    const params: { [key: string]: string } = {};
+  execute<TResult>(pattern: string, url: string): TResult {
     const patternParts = pattern.split("/");
     const urlParts = url.split("/");
+
+    if (patternParts.length !== urlParts.length) return {} as TResult;
+
+    const params: { [key: string]: string } = {};
 
     for (let i = 0; i < patternParts.length; i++) {
       if (patternParts[i].charAt(0) === ":" && urlParts[i]) {
@@ -11,7 +14,7 @@ class ParamExtractor {
       }
     }
 
-    return params;
+    return params as TResult;
   }
 }
 
--- packages/server/src/utils/__tests__/ParamExtractor.test.ts
+++ packages/server/src/utils/__tests__/ParamExtractor.test.ts
@@ -1,11 +1,24 @@
 import ParamExtractor from "../ParamExtractor";
 
 describe("ParamExtractor.ts", () => {
-  it("should extract params correctly", () => {
-    const paramExtractor = new ParamExtractor();
+  let paramExtractor: ParamExtractor;
 
-    const params = paramExtractor.execute("/users/:id/:username", "/users/123/username");
+  beforeEach(() => {
+    paramExtractor = new ParamExtractor();
+  });
+
+  it("should extract params correctly", () => {
+    const params = paramExtractor.execute<{ id: string; username: string }>(
+      "/users/:id/:username",
+      "/users/123/username",
+    );
 
     expect(params).toEqual({ id: "123", username: "username" });
+  });
+
+  it("should handle invalid pattern correctly", () => {
+    const invalidPatternParams = paramExtractor.execute<{}>("/users:id", "users");
+
+    expect(invalidPatternParams).toEqual({});
   });
 });
